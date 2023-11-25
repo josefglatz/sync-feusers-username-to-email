dc78130ff6e905b55f951344fbacb630e330a070
Josef Glatz <typo3@josefglatz.at>
2023-11-25 10:38:17 +0100

    [TASK] Update description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Sync fe_users username to email',
-    'description' => 'This TYPO3 extension takes care of syncing the fe_users.username to fe_users.email field when editing frontend users in the backend',
+    'description' => 'This TYPO3 extension takes care of syncing the fe_users.username to fe_users.email field when editing websites users in the TYPO3 backend',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
